b478ff27494297538836ba62c84351d27cbe2b80
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-02-21 22:38:13 -0300

    basic, -a, -m, -am functional as for as i tested

--- touch.c
+++ touch.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <utime.h>
+#include <sys/stat.h>
 
 int main (int argc, char** argv) {
+    setlocale(LC_ALL, "");
     FILE* fileptr;
     struct optionsBitField {
         unsigned a:1;
@@ -16,44 +21,56 @@ int main (int argc, char** argv) {
         unsigned any:1;
     } modifiers;
     modifiers.a = 0; modifiers.m = 0; modifiers.c = 0; modifiers.t = 0; modifiers.h = 0; modifiers.r = 0; modifiers.d = 0; modifiers.any = 0;
+    short modnum = 0;
     // getting the modifiers cuz touch touches in different ways
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
-            if (argv[i][1] == 'a') {
+            if (argv[i][1] == 'a' && strlen(argv[i]) >= 2) {
                 modifiers.a = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][2] == 'a') {
+            if (argv[i][1] == 'm' && strlen(argv[i]) >= 2) {
+                modifiers.m = 1;
+                modifiers.any = 1;
+                modnum++;
+            }
+            if (argv[i][1] == 'm' && argv[i][2] == 'a' && strlen(argv[i]) >= 3) {
                 modifiers.a = 1;
-                modifiers.any = 1;
-            }
-            if (argv[i][1] == 'm') {
                 modifiers.m = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][2] == 'm') {
+            if (argv[i][1] == 'a' && argv[i][2] == 'm' && strlen(argv[i]) >= 3) {
+                modifiers.a = 1;
                 modifiers.m = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][1] == 'c') {
+            if (argv[i][1] == 'c' && strlen(argv[i]) >= 2) {
                 modifiers.c = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][1] == 't') {
+            if (argv[i][1] == 't' && strlen(argv[i]) >= 2) {
                 modifiers.t = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][1] == 'h') {
+            if (argv[i][1] == 'h' && strlen(argv[i]) >= 2) {
                 modifiers.h = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][1] == 'r') {
+            if (argv[i][1] == 'r' && strlen(argv[i]) >= 2) {
                 modifiers.r = 1;
                 modifiers.any = 1;
+                modnum++;
             }
-            if (argv[i][1] == 'd') {
+            if (argv[i][1] == 'd' && strlen(argv[i]) >= 2) {
                 modifiers.d = 1;
                 modifiers.any = 1;
+                modnum++;
             }
         }
     }
@@ -67,13 +84,46 @@ int main (int argc, char** argv) {
             }
             else {
             if (modifiers.a == 1 && modifiers.m != 1) {
-                
+                for (int i = 1+modnum; i < argc; i++) {
+                    fileptr = fopen(argv[i], "r");
+                    if (fileptr == NULL) fopen(argv[i], "w");
+                    fclose(fileptr);
+                    struct stat tm;
+                    struct utimbuf newtime;
+                    stat(argv[i], &tm);
+                    newtime.actime = time(NULL);
+                    newtime.modtime = tm.st_mtime;
+                    utime(argv[i], &newtime);
+                }
+
             }
             if (modifiers.m == 1 && modifiers.a != 1) {
-
+                for (int i = 1+modnum; i < argc; i++) {
+                    fileptr = fopen(argv[i], "r");
+                    if (fileptr == NULL) fopen(argv[i], "w");
+                    fclose(fileptr);
+                    struct stat tm;
+                    struct utimbuf newtime;
+                    stat(argv[i], &tm);
+                    newtime.actime = tm.st_atime;
+                    newtime.modtime = time(NULL);
+                    utime(argv[i], &newtime);
+                    fclose(fileptr);
+                }
             }
             if (modifiers.a == 1 && modifiers.m == 1) {
-
+                for (int i = 1+modnum; i < argc; i++) {
+                    fileptr = fopen(argv[i], "r");
+                    if (fileptr == NULL) fopen(argv[i], "w");
+                    fclose(fileptr);
+                    struct stat tm;
+                    struct utimbuf newtime;
+                    stat(argv[i], &tm);
+                    newtime.actime = tm.st_atime;
+                    newtime.modtime = tm.st_mtime;
+                    utime(argv[i], &newtime);
+                    fclose(fileptr);
+                }
             }
             if (modifiers.c == 1) {
                 
