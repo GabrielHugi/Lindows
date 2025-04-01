71c1deda537b45817ec05e56ec8796ed1ffb4f48
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-01 15:27:58 -0300

    Ok i fixed some stuff but I need to do a bunch I will do that all later though because I want to do ls command right now

--- touch.c
+++ touch.c
@@ -14,6 +14,8 @@
 THERE ARE MANY OPPORTUNITIES TO CUT DOWN ON GARBAGE REPEATED CODE. LATER MAKE SURE
 TO REMEMBER TO CUT DOWN ON IT AND TO STANDARDIZE SOME OF THE CODE
 ALSO CHECK IF ANY FLAGS ARE MISSING AND THEN CHECK IF EVERYTHINK WORKS
++ 
+MAKE IT SO WITHOUT -H WHEN SOMEONE TRIES TO ALTER LINK DATES IT ACTUALLY ALTERS WHAT IT POINTS TO
 
 *******************************************************************************/
 
@@ -96,6 +98,7 @@ int main (int argc, char** argv) {
     }
     // now the real deal
     // wtf does this for loop do? imma prob take thsi bad boy down later
+    // please remove this stupid ass for loop at some point me
     for (int i = 1; i < argc; i++) {
             // help
             if (argc == 1) {
@@ -118,16 +121,7 @@ int main (int argc, char** argv) {
                     utime(argv[2], &newtime);
                 }
             }
-            if (modifiers.h == 1 && modifiers.t != 1) {
-                /*
-                https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
-                https://doc.embedded-wizard.de/handle-type
-                https://stackoverflow.com/questions/23621778/how-to-find-target-of-symbolic-link-in-windows-c-program
-                https://devblogs.microsoft.com/oldnewthing/20100212-00/?p=14963
-                https://superuser.com/questions/496092/view-a-list-of-symbolic-links-on-system
-                https://cboard.cprogramming.com/c-programming/126660-distinguish-symbolic-link-file-stat-function.html
-                                                
-                */
+            /*if (modifiers.h == 1 && modifiers.t != 1) {
                 for (int i = 1+modnum; i < argc; i++) {
                     fileptr = fopen(argv[i], "r");
                     if (fileptr != NULL) {
@@ -151,126 +145,114 @@ int main (int argc, char** argv) {
                     }
                 }
                 return 0;
-            }
+            }*/
 
-            if (modifiers.h == 1 && modifiers.t == 1) {
+            if (modifiers.h == 1) {
                 int pos = 1+modnum;
                 int size = strlen(argv[pos]);
-                short year = 2000, month = 0, day = 0, hour = 0, minute = 0, second = 0;
-                time_t epochconv;
-                struct tm resulttime;
-                struct _stat tmm;
-                _stat(argv[pos+1], &tmm);
-                struct utimbuf sendtime;
-                if (pos < argc) {
-                    //MMddhhmm 
-                    if(size == 8) {
-                        month += (argv[pos][0]-48)*10; month += (argv[pos][1]-48);
-                        day += (argv[pos][2]-48)*10; day += (argv[pos][3]-48);
-                        hour += (argv[pos][4]-48)*10; hour += (argv[pos][5]-48);
-                        minute += (argv[pos][6]-48)*10; minute += (argv[pos][7]-48);
-                    }
-                    if (size > 8) {
-                        // [yy] || [[cc]yy] || full
-                        if (argv[pos][0] = '[') {
-                            // [[cc]yy]
-                            if (argv[pos][1] = '[') {
-                                // full
-                                if (size == 21) {
-                                    year = (argv[pos][2]-48) * 1000; year += (argv[pos][3]-48) * 100;
-                                    year += (argv[pos][5]-48)*10; year += (argv[pos][6]-48);
-                                    month += (argv[pos][8]-48)*10; month += (argv[pos][9]-48);
-                                    day += (argv[pos][10]-48)*10; day += (argv[pos][11]-48);
-                                    hour += (argv[pos][12]-48)*10; hour += (argv[pos][13]-48);
-                                    minute += (argv[pos][14]-48)*10; minute += (argv[pos][15]-48);
-                                    second += (argv[pos][18]-48)*10; second += (argv[pos][19]-48);
-                                }
-                            }
-                            else {
-                                // [yy] + [.ss]
-                                if (size == 17) {
-                                    year += (argv[pos][1]-48)*10; year += (argv[pos][2]-48);
-                                    month += (argv[pos][4]-48)*10; month += (argv[pos][5]-48);
-                                    day += (argv[pos][6]-48)*10; day += (argv[pos][7]-48);
-                                    hour += (argv[pos][8]-48)*10; hour += (argv[pos][9]-48);
-                                    minute += (argv[pos][10]-48)*10; minute += (argv[pos][11]-48);
-                                    second += (argv[pos][14]-48)*10; second += (argv[pos][15]-48);
-                                }
-                                //[yy]
-                                if (size == 12) {
-                                    year += (argv[pos][1]-48)*10; year += (argv[pos][2]-48);
-                                    month += (argv[pos][4]-48)*10; month += (argv[pos][5]-48);
-                                    day += (argv[pos][6]-48)*10; day += (argv[pos][7]-48);
-                                    hour += (argv[pos][8]-48)*10; hour += (argv[pos][9]-48);
-                                    minute += (argv[pos][10]-48)*10; minute += (argv[pos][11]-48);
-                                }
-                            }
-                        }
-                        // [.ss]
-                        else if(size == 13) {
+                if (modifiers.t == 1) {
+                    short year = 2000, month = 0, day = 0, hour = 0, minute = 0, second = 0;
+                    time_t epochconv;
+                    struct tm resulttime;
+                    struct _stat tmm;
+                    _stat(argv[pos+1], &tmm);
+                    struct utimbuf sendtime;
+                    if (pos < argc) {
+                        //MMddhhmm 
+                        if(size == 8) {
                             month += (argv[pos][0]-48)*10; month += (argv[pos][1]-48);
                             day += (argv[pos][2]-48)*10; day += (argv[pos][3]-48);
                             hour += (argv[pos][4]-48)*10; hour += (argv[pos][5]-48);
                             minute += (argv[pos][6]-48)*10; minute += (argv[pos][7]-48);
-                            second += (argv[pos][10]-48)*10; second += (argv[pos][11]-48);
+                        }
+                        if (size > 8) {
+                            // [yy] || [[cc]yy] || full
+                            if (argv[pos][0] = '[') {
+                                // [[cc]yy]
+                                if (argv[pos][1] = '[') {
+                                    // full
+                                    if (size == 21) {
+                                        year = (argv[pos][2]-48) * 1000; year += (argv[pos][3]-48) * 100;
+                                        year += (argv[pos][5]-48)*10; year += (argv[pos][6]-48);
+                                        month += (argv[pos][8]-48)*10; month += (argv[pos][9]-48);
+                                        day += (argv[pos][10]-48)*10; day += (argv[pos][11]-48);
+                                        hour += (argv[pos][12]-48)*10; hour += (argv[pos][13]-48);
+                                        minute += (argv[pos][14]-48)*10; minute += (argv[pos][15]-48);
+                                        second += (argv[pos][18]-48)*10; second += (argv[pos][19]-48);
+                                    }
+                                }
+                                else {
+                                    // [yy] + [.ss]
+                                    if (size == 17) {
+                                        year += (argv[pos][1]-48)*10; year += (argv[pos][2]-48);
+                                        month += (argv[pos][4]-48)*10; month += (argv[pos][5]-48);
+                                        day += (argv[pos][6]-48)*10; day += (argv[pos][7]-48);
+                                        hour += (argv[pos][8]-48)*10; hour += (argv[pos][9]-48);
+                                        minute += (argv[pos][10]-48)*10; minute += (argv[pos][11]-48);
+                                        second += (argv[pos][14]-48)*10; second += (argv[pos][15]-48);
+                                    }
+                                    //[yy]
+                                    if (size == 12) {
+                                        year += (argv[pos][1]-48)*10; year += (argv[pos][2]-48);
+                                        month += (argv[pos][4]-48)*10; month += (argv[pos][5]-48);
+                                        day += (argv[pos][6]-48)*10; day += (argv[pos][7]-48);
+                                        hour += (argv[pos][8]-48)*10; hour += (argv[pos][9]-48);
+                                        minute += (argv[pos][10]-48)*10; minute += (argv[pos][11]-48);
+                                    }
+                                }
+                            }
+                            // [.ss]
+                            else if(size == 13) {
+                                month += (argv[pos][0]-48)*10; month += (argv[pos][1]-48);
+                                day += (argv[pos][2]-48)*10; day += (argv[pos][3]-48);
+                                hour += (argv[pos][4]-48)*10; hour += (argv[pos][5]-48);
+                                minute += (argv[pos][6]-48)*10; minute += (argv[pos][7]-48);
+                                second += (argv[pos][10]-48)*10; second += (argv[pos][11]-48);
+                            }
                         }
                     }
-                }
-                resulttime.tm_year = year-1900;
-                resulttime.tm_mon = month-1;
-                resulttime.tm_mday = day;
-                if (hour == 24) hour = 0;
-                resulttime.tm_hour = (hour-1);
-                resulttime.tm_min = minute;
-                resulttime.tm_sec = second;
-                resulttime.tm_isdst = -1;
-                epochconv = mktime(&resulttime);
-                // I dont know if this is how it works on linux im just guessing but either way it
-                // seems like good behavior to me and im too lazy to hop off windows to test it out
-                if (modnum == 1) { //means this is the only mod
-                sendtime.actime = epochconv;
-                sendtime.modtime = epochconv;
-                }
-                if (modifiers.a == 1) {
-                    sendtime.actime = epochconv;
-                    sendtime.modtime = tmm.st_mtime;
-                }
-                if (modifiers.m == 1) {
-                    if (modifiers.a != 1) sendtime.actime = tmm.st_atime;
-                    sendtime.modtime = epochconv;
-                }
-                utime(argv[pos+1], &sendtime);
-            }
-            else {
-            if ((modifiers.a == 1 && modifiers.t != 1) || (modifiers.m == 1 && modifiers.t != 1)) {
-                for (int i = 1+modnum; i < argc; i++) {
-                    fileptr = fopen(argv[i], "r");
-                    if (fileptr == NULL && modifiers.c == 0) { 
-                        fileptr = fopen(argv[i], "w");
+                    resulttime.tm_year = year-1900;
+                    resulttime.tm_mon = month-1;
+                    resulttime.tm_mday = day;
+                    if (hour == 24) hour = 0;
+                    resulttime.tm_hour = (hour-1);
+                    resulttime.tm_min = minute;
+                    resulttime.tm_sec = second;
+                    resulttime.tm_isdst = -1;
+                    epochconv = mktime(&resulttime);
+                    if ((modifiers.a == 0 && modifiers.m == 0) || (modifiers.a == 1 && modifiers.m == 1)) {
+                        sendtime.actime = epochconv;
+                        sendtime.modtime = epochconv;
                     }
-                    if (fileptr != NULL) {
-                        fclose(fileptr);
-                        struct stat tm;
-                        struct utimbuf newtime;
-                        stat(argv[i], &tm);
-                        if (modifiers.a == 1) {
-                            newtime.actime = time(NULL);
-                            newtime.modtime = tm.st_mtime;
-                        }
-                        if (modifiers.m == 1) {
-                            newtime.actime = tm.st_atime;
-                            newtime.modtime = time(NULL);
-                        }
-                        if (modifiers.m == 1 && modifiers.a ==1) {
-                            newtime.actime = time(NULL);
-                            newtime.modtime = time(NULL);
-                        }
-                        utime(argv[i], &newtime);
+                    if (modifiers.a == 1 && modifiers.m == 0) {
+                        sendtime.actime = epochconv;
+                        sendtime.modtime = tmm.st_mtime;
                     }
+                    if (modifiers.m == 1 && modifiers.a == 0) {
+                        sendtime.actime = tmm.st_atime;
+                        sendtime.modtime = epochconv;
+                    }
+                    utime(argv[pos+1], &sendtime);
                 }
-
-            }
-            if (modifiers.t == 1) {
+                else {
+                    struct _stat tm;
+                    struct utimbuf newtime;
+                    _stat(argv[pos], &tm);
+                    if (modifiers.a == 1) {
+                        newtime.actime = time(NULL);
+                        newtime.modtime = tm.st_mtime;
+                    }
+                    if (modifiers.m == 1) {
+                        newtime.actime = tm.st_atime;
+                        newtime.modtime = time(NULL);
+                    }
+                    if ((modifiers.m == 1 && modifiers.a == 1) || (modifiers.m == 0 && modifiers.a == 0)) {
+                        newtime.actime = time(NULL);
+                        newtime.modtime = time(NULL);
+                    }  
+                    utime(argv[pos], &newtime);
+                }
+            if (modifiers.t == 1 && modifiers.h != 1) {
                 int pos = 1+modnum;
                 int size = strlen(argv[pos]);
                 short year = 2000, month = 0, day = 0, hour = 0, minute = 0, second = 0;
@@ -371,7 +353,7 @@ int main (int argc, char** argv) {
                         stat(argv[2], &tm);
                         newtime.actime = tm.st_atime;
                         newtime.modtime = tm.st_mtime;
-                        utime(argv[i], &newtime);
+                        utime(argv[3], &newtime);
                     }
                 }
             }
