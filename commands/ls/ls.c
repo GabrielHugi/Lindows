7e2138c1b7bf7d52675c4fd3fc43ccab8e86c471
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-10 17:27:25 -0300

    ee

---
+++ commands/ls/ls.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv) {
+    setlocale(LC_ALL, "");
+    WIN32_FIND_DATA findData;
+    HANDLE fileHandler = NULL;
+
+    char ath[2048]
+}
