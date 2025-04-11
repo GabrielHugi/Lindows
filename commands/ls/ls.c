23b751abe4f58212beaddff65fe4f82a59940d36
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-11 16:49:05 -0300

    Ok its almost finished a for basic Now I will search for flags and other shit and make it look more like linux ls

--- commands/ls/ls.c
+++ commands/ls/ls.c
@@ -15,27 +15,50 @@ int ls (char* dirString) {
         printf("Path not found: [%s]\n", dirString);
         return 0;
     }
+    //console text formatting
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+    // https://learn.microsoft.com/pt-br/windows/console/console-screen-buffers#character-attributes
+    /* Save current attributes */
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
     do
     {
         if(strcmp(findData.cFileName, ".") != 0
                 && strcmp(findData.cFileName, "..") != 0)
         {
-            sprintf(pathString, "%s\\%s", dirString, findData.cFileName);
-
+            // if full path then
+            //sprintf(pathString, "%s\\%s", dirString, findData.cFileName);
+            //else
+            sprintf(pathString, "%s", findData.cFileName);
+            
             if(findData.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
-            {
+            {         
+                SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
                 printf("Directory: %s\n", pathString);
-                ls(pathString);
+                SetConsoleTextAttribute(hConsole, saved_attributes);
+                //ls(pathString); activate this at any case idk maybe later i will need for listing everything that falls under the directory
             }
             else{
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
                 printf("File: %s\n", pathString);
+                SetConsoleTextAttribute(hConsole, saved_attributes);
             }
         }
     }
     while(FindNextFile(fileHandler, &findData));
+    return 0;
 }
 
 int main (int argc, char** argv) {
     setlocale(LC_ALL, "");
-    ls("C:\\Users\\20240192\\Downloads");
+    if (argc == 1) {
+        DWORD len = 2048;
+        LPTSTR dir;
+        GetCurrentDirectory(len, dir);
+        ls(dir);
+    }
+    ls(argv[2]);
 }
