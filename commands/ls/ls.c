528bb939a5f0ce27fe455854cd97d9ca0f01efc0
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-11 15:42:25 -0300

    save

--- commands/ls/ls.c
+++ commands/ls/ls.c
@@ -4,18 +4,10 @@
 #include <locale.h>
 
 int ls (char* dirString) {
-
-}
-
-int main (int argc, char** argv) {
-    setlocale(LC_ALL, "");
     WIN32_FIND_DATA findData;
     HANDLE fileHandler = NULL;
 
     char pathString[2048];
-    char dirString[2048];
-    // for now for testing n shit
-    sprintf(dirString, "%s", "C:\\Users\\20240192\\Downloads");
     sprintf(pathString, "%s\\*.*", dirString);
 
     if((fileHandler = FindFirstFile(pathString, &findData)) == INVALID_HANDLE_VALUE)
@@ -23,4 +15,27 @@ int main (int argc, char** argv) {
         printf("Path not found: [%s]\n", dirString);
         return 0;
     }
+    do
+    {
+        if(strcmp(findData.cFileName, ".") != 0
+                && strcmp(findData.cFileName, "..") != 0)
+        {
+            sprintf(pathString, "%s\\%s", dirString, findData.cFileName);
+
+            if(findData.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
+            {
+                printf("Directory: %s\n", pathString);
+                ls(pathString);
+            }
+            else{
+                printf("File: %s\n", pathString);
+            }
+        }
+    }
+    while(FindNextFile(fileHandler, &findData));
+}
+
+int main (int argc, char** argv) {
+    setlocale(LC_ALL, "");
+    ls("C:\\Users\\20240192\\Downloads");
 }
