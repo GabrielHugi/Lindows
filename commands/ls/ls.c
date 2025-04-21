d8f04e91db76357c8b7c3e64b61846378254f45a
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-21 15:42:41 -0300

    a bit of work before i got lazy

--- commands/ls/ls.c
+++ commands/ls/ls.c
@@ -3,13 +3,17 @@
 #include <stdlib.h>
 #include <locale.h>
 
+//original source code that this was based on for reference so i dont lose the link
+// https://stackoverflow.com/questions/2314542/listing-directory-contents-using-c-and-windows
+
 int ls (char* dirString) {
     WIN32_FIND_DATA findData;
     HANDLE fileHandler = NULL;
 
     char pathString[2048];
+    // passes the "printf style" formatted string to variable in first argument
     sprintf(pathString, "%s\\*.*", dirString);
-
+    // if path doesn't exist then nevermind it
     if((fileHandler = FindFirstFile(pathString, &findData)) == INVALID_HANDLE_VALUE)
     {
         printf("Path not found: [%s]\n", dirString);
@@ -25,16 +29,16 @@ int ls (char* dirString) {
     saved_attributes = consoleInfo.wAttributes;
 
     do
-    {
-        if(strcmp(findData.cFileName, ".") != 0
-                && strcmp(findData.cFileName, "..") != 0)
+    {   
+        //ignore the first bullshit files that arent files
+        if(strcmp(findData.cFileName, ".") != 0 && strcmp(findData.cFileName, "..") != 0)
         {
-            // if full path then
+            // if full path then ( i have no idea what this comment is but it's probably wrong and i just didnt understand this shi when i put this here so imma delete it later after revision n shi)
             //sprintf(pathString, "%s\\%s", dirString, findData.cFileName);
             //else
             sprintf(pathString, "%s", findData.cFileName);
             
-            if(findData.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
+            if(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
             {         
                 SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
                 printf("Directory: %s\n", pathString);
